fa804aeced1e7fe414b22e69f44947d2ef5fb59c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-17 21:27:26 -0600

    Add query selected classes

---
+++ main.js
@@ -0,0 +1,8 @@
+var studyButton = document.querySelector('.study');
+var meditateButton = document.querySelector('.meditate');
+var exerciseButton = document.querySelector('.exercise');
+var taskInput = document.querySelector('.task-text');
+var minutesInput = document.querySelector('.minutes-input');
+var secondsInput = document.querySelector('.seconds-input');
+var submitButton = document.querySelector('.submit-btn');
+
